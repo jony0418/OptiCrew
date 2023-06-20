a7b142c6ff9b90f1bd7429534ded1af83a940b6b
jony0418 <78985884+jony0418@users.noreply.github.com>
2023-06-19 21:03:56 -0600

    adding the tables assist, employee, incident and user, also modify the index models

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
