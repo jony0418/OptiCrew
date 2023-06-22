940e4c8ede468657a91598d779ec19e0058e73fb
AlexTrejo92 <arq.alextrejo@gmail.com>
2023-06-21 18:56:55 -0600

    started working on handlebar templates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,17 @@ const router = express.Router();
 // Define routes
 router.get('/', (req, res) => {
   // Handle the home route logic
+  res.render('index')
+});
+
+// Gets all employees currently registered on the DB (For Administrators Only)
+router.get('/employee', async (req, res)=>{
+  return res.render('employee')
+})
+
+router.get('/login', (req, res) => {
+  // Handle the home route logic
+  res.render('login')
 });
 
 // Export the router
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const routes = require('./controllers');
-
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
 });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
