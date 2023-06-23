07b2f9d712522ec7a64efd3b5e3c9fb9432179ab
ChemaKing13 <jm2000ho@gmail.com>
2023-06-22 20:02:09 -0600

    /assist/:id function upgraded

--- controllers/api/assist-routes.js
+++ controllers/api/assist-routes.js
@@ -1,34 +1,33 @@
 const express = require('express');
-const router = express.Router();
+const router = require('express').Router(); 
 const { Assist, Employee, Incident } = require('../../models');
 
-router.get('/assist/:id', (req, res) => {
-  Assist.findOne({
-    where: {
-      id_employee: req.params.id
-    },
-    include: [
-      { //employee name
-        model: Employee,
-        attributes: ['name']
-      },
-      { //incident date and type if there is
-        model: Incident,
-        attributes: ['date', 'type']
-      }
-    ]
-  })
-  .then(assistData => {
+router.get('/assist/:id', async (req, res) => {
+  try {
+    const assistData = await Assist.findByPk(req.params.id, {
+      include: [
+        {
+          //employee name
+          model: Employee,
+          attributes: ['name']
+        },
+        {
+          model: Incident, 
+          attributes: ['date', 'type']
+        }
+      ]
+    }); 
     if (!assistData) {
-      res.status(404).json({ message: 'No asistencia encontrada para este empleado.' });
-      return;
+      res.status(404).json({ message: 'No assist founded for this employee' }); 
+      return; 
     }
-    res.json(assistData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-});
+    res.json(assistData); 
+  } catch (err) {
+    console.log(err); 
+    res.status(500).json(err); 
+  }
+}); 
+
+
 
 module.exports = router;
