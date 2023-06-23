8a7975548e3f61d94e16f249f9e6f8251a659f9e
Erik <3rik@MacBook-Air-2.local>
2023-06-22 21:19:41 -0600

    route for user and user handlebar

--- controllers/index.js
+++ controllers/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 //import routes
 const homeRoutes = require('./home-routes'); 
 const employeeRoutes = require('./api/employee-routes');
+const userRoutes = require('./api/users-routes');
 
 router.use('/', homeRoutes);
 router.use('/employee', employeeRoutes); 
+router.use('/user', userRoutes); 
 
 
 //exports the router
--- models/employee.js
+++ models/employee.js
@@ -5,108 +5,132 @@ class Employee extends Model {}
 
 Employee.init(
   {
+    // Employee ID
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+    // Assistant ID
     assit: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
+    // Employee first name
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee last name
     lastName: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee shift
     shift: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee gender
     gender: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee birth date
     birth: {
       type: DataTypes.DATEONLY,
       allowNull: false,
     },
+    // Employee address
     address: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee county
     county: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee zip code
     zipCode: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee city
     city: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee cellphone number
     cel: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee education level
     education: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // ID of the department the employee belongs to
     id_department: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'department',
-          key: 'id',
-        },
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'department',
+        key: 'id',
       },
+    },
+    // Employee supervisor
     supervisor: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee position
     position: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee admission date
     admissionDate: {
       type: DataTypes.DATEONLY,
       allowNull: false,
     },
+    // Number of vacation days for the employee
     vacations: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
+    // Employee code
     code: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee active status
     active: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
-      defaultValue: true
+      defaultValue: true,
     },
+    // Employee social security number
     ssn: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee income
     income: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
     },
+    // Employee type
     type: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Employee class
     class: {
       type: DataTypes.STRING,
       allowNull: false,
-    }
+    },
   },
   {
     sequelize,
@@ -118,3 +142,4 @@ Employee.init(
 );
 
 module.exports = Employee;
+
