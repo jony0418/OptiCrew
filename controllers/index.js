45629620c08f00416622fb3d204153733ab0eedf
ChemaKing13 <jm2000ho@gmail.com>
2023-06-22 19:09:34 -0600

    assist routes added to the index.js

--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,11 @@ const router = require('express').Router();
 //import routes
 const homeRoutes = require('./home-routes'); 
 const employeeRoutes = require('./api/employee-routes');
+const assistRoutes = require('./api/assist-routes'); 
 
 router.use('/', homeRoutes);
 router.use('/employee', employeeRoutes); 
-
+router.use('/assist', assistRoutes); 
 
 //exports the router
 module.exports = router; 
