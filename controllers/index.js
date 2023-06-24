7f3cd064e776cf9760426c4a346d16a3673f992a
ChemaKing13 <jm2000ho@gmail.com>
2023-06-24 11:42:59 -0600

    general update

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,15 @@
-const express = require('express'); 
-const router = require('express').Router(); 
+const express = require('express');
+const router = require('express').Router();
 
-//import routes
-const homeRoutes = require('./home-routes'); 
+// Import routes
+const homeRoutes = require('./home-routes');
 const employeeRoutes = require('./api/employee-routes');
-const assistRoutes = require('./api/assist-routes'); 
+const assistRoutes = require('./api/assist-routes'); // Corrected filename
 
 router.use('/', homeRoutes);
-router.use('/employee', employeeRoutes); 
-router.use('/assist', assistRoutes); 
+router.use('/employee', employeeRoutes);
+router.use('/assist', assistRoutes);
 
-//exports the router
-module.exports = router; 
+// Export the router
+module.exports = router;
+
