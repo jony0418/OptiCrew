5add4f3c12d53b69d58ccb37bf713e24ac5c64d6
AlexTrejo92 <arq.alextrejo@gmail.com>
2023-06-25 12:02:12 -0600

    implementation of hamburger Icon, addition of add employee, frontend work

--- public/js/employee.js
+++ public/js/employee.js
@@ -3,10 +3,11 @@ const printAllEmployees = async (event) => {
     console.log('this will print all employees')
     const response = await fetch('/api/employee', {
         method: 'GET',
-        body: JSON.stringify(),
+        // body: JSON.stringify(),
         headers: {'Content-Type': 'application/json'}
     })
     console.log('response:', response)
+    console.log('response:', response.name)
 };
 
 const newEmployeeBtn = document.getElementById('appBtn');
